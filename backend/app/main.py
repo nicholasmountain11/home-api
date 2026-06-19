d1a235c2eef060d780886455f2010abae3a404bc
Nicholas Mountain <nmountain@unc.edu>
2026-06-19 00:16:53 -0400

    remove last traces of sensor service name

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,18 +3,14 @@ from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI
 
 from services.connection import ConnectionService
-from services.actuator import ActuatorService
 
 sensor_service: ConnectionService
-actuator_service: ActuatorService
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global sensor_service
-    global actuator_service
-    sensor_service = ConnectionService(sensor_port=8001, actuator_port=8002)
-    actuator_service = ActuatorService(port=8002)
+    global connection_service
+    connection_service = ConnectionService(sensor_port=8001, actuator_port=8002)
     yield
 
 
@@ -28,11 +24,11 @@ def read_root():
 
 @app.get("/get_sensor_message/{nickname}")
 def get_sensor_message(nickname: str):
-    message = sensor_service.get_message(nickname=nickname)
+    message = connection_service.get_message(nickname=nickname)
     return {"Message": message}
 
 
 @app.get("/list_sensors", response_model=list[str])
 def list_sensors() -> list[str]:
-    sensors = sensor_service.registry.keys()
+    sensors = connection_service.registry.keys()
     return sensors
